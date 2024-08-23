dbde5390b6de3a1288971f99eff227c01cf56375
3nzoo <florenzodinglasan@gmail.com>
2024-08-23 23:53:40 +0800

    fix next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'keaproof.s3.ap-southeast-1.amazonaws.com',
-      port: '',
-      pathname: '/**',
-    },
-  ],
-};
+const nextConfig = {};
 
 export default nextConfig;
